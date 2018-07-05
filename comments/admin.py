45c0af9bcede8957b053c6062901e188a288ce9b
Hard-RUM <767231157@qq.com>
2018-07-05 21:38:11 +0800

    小改动

--- comments/admin.py
+++ comments/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Comment
 
 # Register your models here.
-# 为了让 admin 界面管理某个数据模型，我们需要先注册该数据模型到 admin。比如，我们之前在 Model 中已经创建了模型 Comment
+# 为了让 admin 界面管理某个数据模型，需要先注册该数据模型到 admin。比如，之前在 Model 中已经创建了模型 Comment
 admin.site.register(Comment)
